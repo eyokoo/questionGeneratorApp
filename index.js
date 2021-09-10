d5c4d9988eeac7a8a6609b3426d10dd303ffc9c9
eyokoo <emi.yokoo94@gmail.com>
2021-09-10 10:28:03 -0500

    push changes

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(require("./routes/questions")); //importing the routes module
 
-
+app.get('/', (req, res) => {
+  res.send("Welcome to my Server")
+});
 
 
 const PORT = process.env.PORT || 9000
